cf8c5e78a6b5229cb5c8bd59cbb7d09942fb2b90
Bill Niblock <bill_niblock@yahoo.com>
2015-11-07 17:00:15 -0500

    Modify configs
    - Updated vimrc to point to in-repo files, for Fugitive
    - Modified tmux status bar to include better details
         - Created clocks.sh to show multiple timezones
         - Added to right-side of tmux status bar

---
+++ tmux/clocks.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Supply tmux with times for right-hand status bar
+# Each variable encompasses a time and a color
+
+caliTime="$(TZ=America/Los_Angeles date +%R)"
+buffTime="$(TZ=America/New_York date +%R)"
+utcTime="$(date -u +%R)"
+bangTime="$(TZ=Asia/Kolkata date +%R)"
+
+caliForm="#[fg=red][ $caliTime ]"
+buffForm="#[fg=green][ $buffTime ]"
+utcForm="#[fg=blue][ $utcTime ]"
+bangForm="#[fg=magenta][ $bangTime ]"
+
+echo $caliForm$buffForm$utcForm$bangForm
