aff1537f1423f596beacf7d47318252ca8c9cf03
Bill Niblock <bill_niblock@yahoo.com>
2016-04-28 13:49:53 -0400

    Minor update

---
+++ tmux/clocks.rb
@@ -0,0 +1,47 @@
+######
+# Clocks -- Tmux status line output
+###
+
+require 'net/http'
+
+class Weather
+
+     attr_reader :temp, :flag, :summary
+
+     def initialize()
+          @temp = 68
+          @flag = "(!)" if hasflag?
+          @summary = "Dang nice"
+     end
+
+     private
+
+     def get_weather(lat = 42.9333, lon = -78.877)
+
+     end
+
+     def hasflag?
+          return true
+     end
+
+end
+
+class Clocks
+
+     def initialize()
+          w = Weather.new()
+          t = Time.new()
+          @tu = t.utc.strftime("[ %H:%M ]")
+          @tb = t.getlocal("-04:00").strftime("<][ %b %d %H:%M | ")
+          @tw = "#{w::flag} #{w::temp} ][>"
+          @tc = t.getlocal("-07:00").strftime("<][ %H:%M ]")
+          @ti = t.getlocal("+05:30").strftime("[ %H:%M ][>")
+     end
+
+     def show()
+          puts "#{@tc} #{@tb}#{@tw} #{@tu} #{@ti}"
+     end
+end
+
+c = Clocks.new()
+c.show()
--- tmux/clocks.sh
+++ tmux/clocks.sh
@@ -4,12 +4,12 @@
 # Each variable encompasses a time and a color
 
 caliTime="$(TZ=America/Los_Angeles date +%R)"
-buffTime="$(TZ=America/New_York date +%R)"
+buffTime="$(TZ=America/New_York date +'%b %d %R')"
 utcTime="$(date -u +%R)"
 bangTime="$(TZ=Asia/Kolkata date +%R)"
 
 caliForm="#[fg=red]<][ $caliTime ] "
-buffForm="#[fg=green]<][ $buffTime ][> "
+buffForm="#[fg=green]<][ $buffTime | #[fg=red](!)#[fg=green]][> "
 utcForm="#[fg=blue][ $utcTime ][> "
 bangForm="#[fg=magenta][ $bangTime ][>>"
 
